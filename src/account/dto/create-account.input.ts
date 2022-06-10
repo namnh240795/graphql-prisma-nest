bd8a3a70965a8a1efe71ab61e2de4945d00b0cca
Nam Nguyen <nam.nh@agiletech.vn>
2022-06-10 11:42:32 +0700

    set up validator done

--- src/account/dto/create-account.input.ts
+++ src/account/dto/create-account.input.ts
@@ -1,3 +1,10 @@
+import { IsEmail, IsNotEmpty, Matches } from 'class-validator';
+
 export class CreateAccountInput {
+  @IsEmail()
+  email: string;
+  @Matches(/^(03[2-9]|05[689]|07[06789]|08[1-9]|09[0-9])([0-9]{7,9})$/)
+  phone: string;
+  @IsNotEmpty()
   password: string;
 }
